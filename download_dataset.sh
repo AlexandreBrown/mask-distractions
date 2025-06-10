fa286efc28d28a5e732c6dad90d17883e2b3231c
AlexandreBrown <alexandrebrown97@gmail.com>
2025-06-10 14:54:42 -0400

    Updated download_dataset script

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 DATA_DIR=./datasets/
 
-if [ -d "$DATA_DIR" ] && [ "$(ls -A "$DATA_DIR")" ]; then
-  echo "Place365 dataset already present in $DATA_DIR; skipping download."
-  exit 0
+if [ -d "${DATA_DIR}places365_standard" ] && [ "$(ls -A "${DATA_DIR}places365_standard")" ]
+then
+  echo "Place365 dataset already present in $DATA_DIR skipping download."
+else
+  echo "Downloading Place365 dataset..."
+  mkdir -p "$DATA_DIR"
+  cd "$DATA_DIR"
+  wget http://data.csail.mit.edu/places/places365/places365standard_easyformat.tar
+  tar -xf places365standard_easyformat.tar
+  rm places365standard_easyformat.tar
+  cd ../../
 fi
-
-echo "Downloading Place365 dataset..."
-mkdir -p "$DATA_DIR"
-cd "$DATA_DIR"
-wget http://data.csail.mit.edu/places/places365/places365standard_easyformat.tar
-tar -xf places365standard_easyformat.tar
-rm places365standard_easyformat.tar
-cd ../../
